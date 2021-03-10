673af19754defe2aca9dcd714123133d3967e47e
Jack Wright <jwrite333@gmail.com>
2021-03-09 16:28:41 -0800

    Added first test for adding and subtracting

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import calculator
+
+class TestCalc:
+	def test_adding(self):
+		assert 9 == calculator.add(1, 8)
+		return
+
+	def test_subtracting(self):
+		assert 8 == calculator.subtract(16, 8)
+		return
