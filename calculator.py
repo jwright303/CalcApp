6448f2b804f76ab9ad4d8fafc96743a0179932f4
Jack Wright <jwrite333@gmail.com>
2021-03-11 08:52:18 -0800

    added . files

--- calculator.py
+++ calculator.py
@@ -1,10 +1,16 @@
 def add(a, b):
-	return a + b
+    return a + b
+
 
 def subtract(a, b):
-	return a - b
+    return a - b
+
 
 def main():
+    add(1, 3)
+    subtract(4, 1)
 
-	return
+    return
+
+
 main()
--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,11 @@
 import calculator
 
-class TestCalc:
-	def test_adding(self):
-		assert 9 == calculator.add(1, 8)
-		return
 
-	def test_subtracting(self):
-		assert 8 == calculator.subtract(16, 8)
-		return
+class TestCalc:
+    def test_adding(self):
+        assert 9 == calculator.add(1, 8)
+        return
+
+    def test_subtracting(self):
+        assert 8 == calculator.subtract(16, 8)
+        return
